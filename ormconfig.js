00e8f74826aa1abef4e1bcdc6861b5278d8178c2
shahiab <abdul.shahid@ofx.com>
2020-09-25 16:33:04 +1000

    orm config changes

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,26 @@
 
 
-module.exports = {
+module.exports = [{
   "name": "local",
   "type": "mysql",
-  "host": process.env.DB_HOST || "localhost",
+  "host": "localhost",
   "port": 3306,
-  "username": process.env.DB_USERNAME || "root",
-  "password": process.env.DB_PASSWORD || "Password1",
+  "username": "root",
+  "password": "Password1",
   "database": process.env.DB_NAME || "MemoriesDB",
   "synchronize": true,
   "logging": true,
   "entities": ["src/entities/*.*"]
-}
+},
+{
+  "name": "production",
+  "type": "mysql",
+  "host": process.env.DB_HOST,
+  "port": 3306,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME || "MemoriesDB",
+  "synchronize": true,
+  "logging": true,
+  "entities": ["src/entities/*.*"]
+}]
