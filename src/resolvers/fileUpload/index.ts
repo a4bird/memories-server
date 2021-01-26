ced0720784cd5211321f0974fc019a950511c537
shahiab <abdul.shahid@ofx.com>
2021-01-27 10:27:41 +1100

    storage bucket name

--- src/resolvers/fileUpload/index.ts
+++ src/resolvers/fileUpload/index.ts
@@ -3,7 +3,7 @@ import { AWSS3Uploader } from 'src/utils/awsS3Uploader';
 const s3Uploader = new AWSS3Uploader({
   accessKeyId: process.env.AWS_ACCESS_KEY!,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  destinationBucketName: 'my-really-cool-bucket',
+  destinationBucketName: 'my-memories-bucket',
 });
 
 const resolvers = {
