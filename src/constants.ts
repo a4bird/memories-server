8e0910e30135404ac495e9c2ea9e2c86ad986837
NSWRO\1163524 <ashahid@woolworths.com.au>
2021-06-19 22:46:46 +1000

    Album bucket

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const BUCKET_NAME = 'my-memories-bucket';
+export const BUCKET_NAME = 'my-memories-bucket'; // TODO this is going to come as an environment variable
 export const PAGE_SIZE = 50;
