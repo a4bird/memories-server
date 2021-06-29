918eb0dbf7ddb85f88fac329875be5e0ac74fd71
NSWRO\1163524 <ashahid@woolworths.com.au>
2021-06-29 18:13:32 +1000

    CICD - WIP

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,10 +1,17 @@
 import { getConnectionOptions, createConnection } from 'typeorm';
+import { UserAccount } from '../entities/userAccount';
+import { UserProfile } from '../entities/userProfile';
+import { Album } from '../entities/album';
+
+// Not relying on glob pattern to add entities because in linux container, this seems to not work
+// would need to spend more time to figure out why
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
   return createConnection({
     ...connectionOptions,
+    entities: [UserAccount, UserProfile, Album],
     name: 'default',
   });
 };
